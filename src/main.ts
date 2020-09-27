2e64faa6900c758d76a7d838e3ee9c2d07e2ccd2
drmrhdt <drmrhdt@gmail.com>
2020-09-27 10:18:22 +0300

    feat(main.ts): set origin to *

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  })
 
   const options = new DocumentBuilder()
     .setTitle('Library')
