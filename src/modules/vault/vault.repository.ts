4d7e7f0ffdb24f54653ffeb9faf8c65115fbec6b
drmrhdt <drmrhdt@gmail.com>
2021-01-03 22:37:01 +0300

    feat(vault.repository.ts): construct vault

--- src/modules/vault/vault.repository.ts
+++ src/modules/vault/vault.repository.ts
@@ -10,7 +10,38 @@ export class VaultRepository extends Repository<Vault> {
   }
 
   async getAllVaults() {
-    return await this.find({ relations: ['books'] })
+    const unfilteredVaults = await this.find({ relations: ['books'] })
+    const filteredVaults = []
+
+    for (
+      let curVaultNum = 0;
+      curVaultNum < unfilteredVaults.length;
+      curVaultNum++
+    ) {
+      const curVault = unfilteredVaults[curVaultNum]
+      curVault.shelfs = []
+      for (let curShelf = 1; curShelf <= curVault.numShelfs; curShelf++) {
+        const shelf = { rows: [] }
+        // get books that located on current shelf
+        const booksOnCurShelf = curVault.books.filter(
+          book => book.shelf === curShelf,
+        )
+        // construct shelf with two rows and books on it sorted by books's ordering
+        for (let curRow = 1; curRow <= curVault.numRows; curRow++) {
+          // get books that located on each row of the current shelf
+          const booksOnCurRow = booksOnCurShelf
+            .filter(book => book.row === curRow)
+            .sort(book => book.number) // sort by order on row
+          const row = { books: [] }
+          row.books.push(...booksOnCurRow)
+          shelf.rows.push(row)
+        }
+        curVault.shelfs.push(shelf)
+      }
+      filteredVaults.push(curVault)
+    }
+
+    return filteredVaults
   }
 
   async findById(id: number) {
