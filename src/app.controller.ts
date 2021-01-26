d065abf78a0ab47bb9d5ce1072f13b94526a6eb8
drmrhdt <drmrhdt@gmail.com>
2021-01-27 00:28:11 +0300

    feat: settings for deploy to Heroku and / method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,4 +4,9 @@ import { AppService } from './app.service'
 @Controller()
 export class AppController {
   constructor() {}
+
+  @Get('/')
+  getDefault(): string {
+    return '<div>API</div>'
+  }
 }
